3e9e8cac2fb7a61b2c96eeaf1c875477ebc45bb8
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-01-04 20:53:34 +0530

    Some minor testing changes

--- scripts/star.sh
+++ scripts/star.sh
@@ -13,6 +13,14 @@ orig=$port
 for ((i=1; i<n;i++))
 do
     port=$((prev + RANDOM))
+    if [ $port -gt 65535 ]
+    then
+        port=$((10001 + port%65535))
+    fi
     id=`node -e "console.log(require('./lib/util').nodeID('$ip', $port))"`
     xt "$id $ip:$port" $ip $port $ip $orig
+    if [ $(( i % 10 )) -eq 0 ]
+    then
+        sleep 1
+    fi
 done
