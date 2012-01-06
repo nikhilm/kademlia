988362da41138fe4ecc0332f5a0470333eb0aa13
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-01-06 11:28:11 +0530

    Keep xterms running so stack traces remain visible

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,5 +2,5 @@
 
 function xt() {
     echo xterm -geometry 100x32 -T "$1" -e node run.js $2:$3 $4:$5 &
-    xterm -geometry 100x32 -T "$1" -e node run.js $2:$3 $4:$5 &
+    xterm -geometry 100x32 -hold -T "$1" -e node run.js $2:$3 $4:$5 &
 }
