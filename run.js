4a500b0a85fb2db28cd6930c902436e90ff6b479
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-03-18 23:44:39 +0530

    Only the master node gets an initial set in the run script

--- run.js
+++ run.js
@@ -8,24 +8,26 @@ var node = new knode.KNode({ address: self[0], port: port });
 if (process.argv.length >= 4) {
     var arg = process.argv[3].split(':');
     if (arg[0])
-        setInterval(function() {
-            node.connect(arg[0], parseInt(arg[1]), function() {
-                node.get('foo', function(err, value) {
-                    if (err) {
-                        console.log("Not found");
-                        node.set('foo', 'bar', function(err) {
-                            node.get('foo', function(err, value) {
-                                if (err)
-                                    console.log("Still not inserted");
-                                else
-                                    console.log("======> Inserted", value);
-                            });
-                        });
-                    }
-                    else {
-                        console.log("=======> Already exists", value);
-                    }
+        node.connect(arg[0], parseInt(arg[1]));
+    setInterval(function() {
+        /*node.get('foo', function(err, value) {
+            if (err) {
+                console.log("Not found");
+                node.set('foo', 'bar', function(err) {
+                    node.get('foo', function(err, value) {
+                        if (err)
+                            console.log("Still not inserted");
+                        else
+                            console.log("======> Inserted", value);
+                    });
                 });
-            });
-        }, 4000);
+            }
+            else {
+                console.log("=======> Already exists", value);
+            }
+        });*/
+        if (port == 10000)
+            node.set('foo', 'bar');
+        console.log("This node storage", node._storage);
+    }, 4000);
 }
